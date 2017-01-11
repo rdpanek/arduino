96dfa4a89876d23032927cf3deae2752f29bc94f
Radim Daniel Panek <rdpanek@gmail.cz>
2017-01-11 21:38:03 +0100

    PanIoT rellay: fix variables for unique name

--- paniot/esp8266/wemosMini/rellay.h
+++ paniot/esp8266/wemosMini/rellay.h
@@ -3,13 +3,13 @@ bool allowRellay = true;
 bool stateRellay = false;
 String onEndpoint = "/rellayOn";
 String offEndpoint = "/rellayOff";
-int dellayMS = 10000;
-int lastOnMS = 0;
-bool allowAutoOff = true;
+int rellayDellayMS = 10000;
+int rellayLastOnMS = 0;
+bool rellayAllowAutoOff = true;
 
 void autoOffRellay() {
-  if (allowAutoOff) {
-    if (millis() > (dellayMS + lastOnMS)) { 
+  if (rellayAllowAutoOff) {
+    if (millis() > (rellayDellayMS + rellayLastOnMS)) { 
       stateRellay = false;
     }  
   }
