327786ebcb1527f2469303982ce93e3b99f01e19
Radim Daniel Panek <rdpanek@gmail.cz>
2017-01-13 13:39:36 +0100

    step1: Serial link example

--- paniot/esp8266/wemosMini/wemosMini.ino
+++ paniot/esp8266/wemosMini/wemosMini.ino
@@ -1,62 +1,12 @@
-String deviceName = "PanIoT-temperature";
-String deviceLocation = "pracovna";
+int i = 0;
 
-#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-ESP8266WebServer server(80);
-
-// senzors
-#include "utility.h"
-#include "oled.h"
-#include "beep.h"
-#include "led.h"
-#include "wifiManagerSetup.h"
-#include "ntp.h"
-#include "elasticsearch.h"
-#include "dht22.h"
-#include "dallas.h"
-#include "ldr.h"
-
-// always on the end
-#include "config.h"
-#include "ota.h"
-#include "webServer.h"
-
-
-// the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-  wifiManagerInit();
-  webServerInit();
-  fsInit();
-  initOTA();
-  loadConfig();
 
-
-  // ntp
-  if(WiFi.status() == WL_CONNECTED) {
-    NTP.begin("pool.ntp.org", 1, true);
-    NTP.setInterval(63);
-  }
-
-  NTP.onNTPSyncEvent([](NTPSyncEvent_t event) {
-    ntpEvent = event;
-    syncEventTriggered = true;
-  });  
-
-  oledInit();
-
-  // vse nastaveno, startuje se
   Serial.println("-- start --");
 }
 
 void loop() {
-  server.handleClient();
-  ArduinoOTA.handle();
-  syncNtp();
-  
-  measureDallas();
-  measureLDR();
-  displayMessage(String(WiFi.RSSI())+" dBm", String((int)dallasTemperature)+" °C", 100);
+  Serial.println(i++);
 }
 
