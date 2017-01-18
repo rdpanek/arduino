2e058fa6b8e9b7e4bd0d4ef6d5744b9bf5de8375
Radim Daniel Panek <rdpanek@gmail.cz>
2017-01-18 14:44:07 +0100

    step15: SPIFFS & ArduinoJSON example

--- paniot/esp8266/wemosMini/wemosMini.ino
+++ paniot/esp8266/wemosMini/wemosMini.ino
@@ -1,53 +1,97 @@
+#include <ArduinoJson.h> // documentation https://github.com/bblanchon/ArduinoJson/wiki/Quick%20Start
+#include <FS.h> // documentation https://github.com/esp8266/Arduino/blob/master/doc/filesystem.md
 
-#include "SSD1306.h"
+String konfiguracniSoubor = "/konfigurace.json";
 
-// custom font generator http://oleddisplay.squix.ch/#/home
-#include "Irish_Growler_20.h"
-#include "Smokum_26.h"
+String nazevZarizeni = "Teplomer";
+int pocetTeplomeru = 1;
+float namerenaTeplota = 25.00;
+
+String formatBytes(size_t bytes){
+  if (bytes < 1024){
+    return String(bytes)+"B";
+  } else if(bytes < (1024 * 1024)){
+    return String(bytes/1024.0)+"KB";
+  } else if(bytes < (1024 * 1024 * 1024)){
+    return String(bytes/1024.0/1024.0)+"MB";
+  } else {
+    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+  }
+}
+
+void saveConfig() {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["nazevZarizeni"] = nazevZarizeni.c_str();
+  json["pocetTeplomeru"] = pocetTeplomeru;
+  json["namerenaTeplota"] = namerenaTeplota;
+  File configFile = SPIFFS.open(konfiguracniSoubor, "w+");
+  if (!configFile) {
+    Serial.println("Nejde otevrit soubor pro ulozeni konfigurace.");
+  }
+  
+  json.printTo(configFile);
+  Serial.println("Ulozeni bylo uspesne.");
+}
 
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
+void loadConfig() {
+  if (!SPIFFS.exists(konfiguracniSoubor)) saveConfig();
+  File configFile = SPIFFS.open(konfiguracniSoubor, "r");
+  if (!configFile) {
+    Serial.println("Nelze otevrit konfiguracni soubor");
+  }
 
-SSD1306 display(0x3c, D2, D1);
+  size_t size = configFile.size();
+  if (size > 1024) {
+    Serial.println("Konfiguracni soubor je prilis velky");
+  }
+  std::unique_ptr<char[]> buf(new char[size]);
+  configFile.readBytes(buf.get(), size);
 
-int16_t displayMarginLeft = 31;
-int16_t displayMarginTop = 13;
-const int pinCidlaDS = D3;
-float dallasTemperature = 0;
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.parseObject(buf.get());
 
-OneWire oneWireDS(pinCidlaDS);
-DallasTemperature dallasDS(&oneWireDS);
+  if (!json.success()) {
+    Serial.println("Chyba v parsovani konfiguracniho souboru");
+  }
 
+  nazevZarizeni = json["nazevZarizeni"];
+  Serial.print("nazevZarizeni: ");
+  Serial.println(nazevZarizeni);
 
-void displayMessage(String text, int _delay) {
-  display.setFont(Smokum_26);
-  display.drawString(64, displayMarginTop + 15, text);
-  display.display();
-  delay(_delay);
-  display.clear();
+  pocetTeplomeru = json["pocetTeplomeru"];
+  Serial.print("pocetTeplomeru: ");
+  Serial.println(pocetTeplomeru);
+
+  namerenaTeplota = json["namerenaTeplota"];
+  Serial.print("namerenaTeplota: ");
+  Serial.println(namerenaTeplota);
+  
+  Serial.println("Nacteni konfigurace bylo uspesne.");
 }
 
 void setup() {
   Serial.begin(115200);
 
-  display.init();
-  display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.flipScreenVertically();
-  display.displayOn();
-  display.drawString(64, displayMarginTop + 15, "Wi-Fi");
-  display.display();
-  delay(2000);
-  display.clear(); 
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {    
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      Serial.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    Serial.printf("\n");
+  }  
+
+  loadConfig();
+
 
   Serial.println("-- start --");
 }
 
 void loop() {
-  dallasDS.requestTemperatures();
-  dallasTemperature = dallasDS.getTempCByIndex(0);
-  displayMessage(String((int)dallasTemperature)+" °C", 200);
 
-  delay(1000);
 } 
 
